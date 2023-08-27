438a663eefcf67066eb74a3c5a5542630bc8ab7d
AlejoLGG <luisalejo866@gmail.com>
2023-08-27 14:41:51 -0500

    Subiendo taller 2 y parcial 1

---
+++ taller2/script.sh
@@ -0,0 +1,15 @@
+#!/bim/bash
+
+variable=${1}
+
+mensajes(){
+
+echo "Hackeando mi casa"
+echo "ENtrando a mi casa"
+echo "Sentandome en mi silla"
+echo " "
+
+echo "El valor insertado es: $variable"
+}
+
+mensajes
